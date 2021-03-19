7380db25e437c58ed4a9bb3dc8c653d7c15f015a
louis <l.j.speelman.1@student.rug.nl>
2021-03-19 11:59:51 +0100

    Added file extension limitations. So not any type of file can be uploaded.

--- GUI/app.py
+++ GUI/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, send_from_directory, redirect, url_for, after_this_request, send_file
+from flask import Flask, render_template, request, send_from_directory, redirect, url_for, after_this_request, \
+    send_file, flash
 import os
 from test_script import *
 
@@ -6,22 +7,29 @@ from test_script import *
 app = Flask(__name__)
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+app.config['SECRET_KEY'] = '\xfd{H\xe5<\x95\xf9\xe3\x96.5\xd1\x01O<!\xd5\xa2\xa0\x9fR"\xa1\xa8'
+ALLOWED_EXTENSION = {'txt', 'srt'}
+app.config.update()
 files_uploaded = 0
+
 
 @app.route('/')
 def index_page():
-    # TODO: Create two upload buttons. One for script and other for SRT.
-    # TODO: Change the upload buttons to the files themselves, files can be replaced if needed! User error?
 
     # Checks if the uploads folder exists, if not create an uploads folder.
     upload_folder = APP_ROOT + '/uploads'
     if not os.path.exists(upload_folder):
         os.makedirs(upload_folder)
-    print(files_uploaded)
+
     return render_template('index.html', file_exist_srt=os.path.isfile(os.path.join(upload_folder, 'subs.srt')),
-                           file_exist_out=os.path.isfile(os.path.join(upload_folder, 'out_test.txt')),
+                           file_exist_out=os.path.isfile(os.path.join(upload_folder, 'out_test.json')),
                            file_exist_script=os.path.isfile(os.path.join(upload_folder, 'script.txt')),
                            total_upload=files_uploaded)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSION
 
 
 @app.route('/upload_srt', methods=['GET', 'POST'])
@@ -33,38 +41,47 @@ def upload_srt():
     if request.method == 'POST':
         f = request.files['file']
         filename = f.filename
-        dest = f'{upload_folder}/{filename}'
-        new_file = f'{upload_folder}/subs.srt'
-        f.save(dest)
-        os.rename(dest, new_file)
-        files_uploaded += 1
+        if allowed_file(filename):
+            dest = f'{upload_folder}/{filename}'
+            new_file = f'{upload_folder}/subs.srt'
+            f.save(dest)
+            os.rename(dest, new_file)
+            files_uploaded += 1
+        else:
+            flash('File extension not allowed!')
         return redirect('/')
 
 
 @app.route('/upload_script', methods=['GET', 'POST'])
 def upload_script():
     global files_uploaded
+
     upload_folder = os.path.join(APP_ROOT, 'uploads')
 
     if request.method == 'POST':
         f = request.files['file']
         filename = f.filename
-        dest = f'{upload_folder}/{filename}'
-        new_file = f'{upload_folder}/script.txt'
-        f.save(dest)
-        os.rename(dest, new_file)
-        files_uploaded += 1
+        if allowed_file(filename):
+            dest = f'{upload_folder}/{filename}'
+            new_file = f'{upload_folder}/script.txt'
+            f.save(dest)
+            os.rename(dest, new_file)
+            files_uploaded += 1
+        else:
+            flash('File extension not allowed!')
         return redirect('/')
 
 
 @app.route('/process')
 def process():
+    global files_uploaded
     # TODO: Make it process script and subs files. Return JSON file.
     upload_folder = os.path.join(APP_ROOT, 'uploads')
-    file_in = os.path.join(upload_folder, 'script.txt')
-    file_out = os.path.join(upload_folder, 'out_test.txt')
-    add_test(file_in, file_out)
-    files_upload = 0
+    srt_in = os.path.join(upload_folder, 'subs.srt')
+    script_in = os.path.join(upload_folder, 'script.txt')
+    file_out = os.path.join(upload_folder, 'out_test.json')
+    add_test(script_in, srt_in, file_out)
+    files_uploaded = 0
     return redirect(url_for('index_page'))
 
 
@@ -72,7 +89,7 @@ def process():
 def download():
     # TODO: Make it download the appropriate output file
     upload_folder = os.path.join(APP_ROOT, 'uploads')
-    file_path = os.path.join(upload_folder, 'out_test.txt')
+    file_path = os.path.join(upload_folder, 'out_test.json')
 
     @after_this_request
     def remove_files(response):
--- GUI/test_script.py
+++ GUI/test_script.py
@@ -1,13 +1,18 @@
 import os
+import json
 
 
-def add_test(file, dest):
-    with open(file, 'r') as f:
-        text_test = f.readlines()
+def add_test(script, srt, dest):
+    with open(script, 'r') as f:
+        script_test = f.readlines()
 
-    new_text = ''.join(text_test) + '1234'
+    with open(srt, 'r') as f:
+        srt_test = f.readlines()
+
+    new_text = ''.join(script_test) + ' ' + ''.join(srt_test)
 
     with open(dest, 'w') as new_f:
-        new_f.write(new_text)
+        new_f.write(json.dumps(new_text))
 
-    os.remove(file)
+    os.remove(script)
+    os.remove(srt)
