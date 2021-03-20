ad6f2feee875ee11a176c71c6d5095472970ecfd
Joey <joeymallat.business@hotmail.com>
2021-03-20 17:12:09 +0100

    output converter added

---
+++ convert_to_output.py
@@ -0,0 +1,26 @@
+import csv
+import json
+
+
+def convert_to_csv(dict):
+    with open('outputs/csv_output.csv', 'w') as csv_file:
+        fieldnames = ['line', 'actor', 'dialogue', 'time']
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames, delimiter=',')
+
+        writer.writeheader()
+        for line in dict:
+            line_number = line.split()[1]
+            actor = dict[line]['actor']
+            line_text = dict[line]['line']
+            time = dict[line]['time']
+            writer.writerow({'line': line_number, 'actor': actor,
+                            'dialogue': line_text, 'time': time})
+
+        return csv_file
+
+
+def convert_to_json(dict):
+    with open('outputs/json_output.json', 'w') as json_file:
+        json_output = json.dump(dict, json_file)
+
+    return json_file
