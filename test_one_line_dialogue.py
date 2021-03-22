d7ab47ecccd520c6fc0aebe99d817aea66673a2a
JoeyMallat <joeymallat.business@hotmail.com>
2021-03-22 16:21:04 +0100

    Fixed a small issue

--- test_one_line_dialogue.py
+++ test_one_line_dialogue.py
@@ -5,8 +5,8 @@ test_content = "This a sentence\n which should be on\n one line."
 expected_output = ['This a sentence\n', ' which should be on\n', ' one line.']
 
 def test_one_line_dialogue():
-	with open("./temp/test_dialogue_file.txt", 'w') as test_file:
+	with open("test_dialogue_file.txt", 'w') as test_file:
 		test_file.write(test_content)
 
-	content = one_line_dialogue.one_line_dialogue("./temp/test_dialogue_file.txt")
+	content = one_line_dialogue.one_line_dialogue("test_dialogue_file.txt")
 	assert content == expected_output
