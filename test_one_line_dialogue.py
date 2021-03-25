c02d42430369ad1f35d48a3524c96b82a1810fce
JoeyMallat <joeymallat.business@hotmail.com>
2021-03-25 23:54:51 +0100

    Corrected unit test

--- test_one_line_dialogue.py
+++ test_one_line_dialogue.py
@@ -2,7 +2,7 @@ import one_line_dialogue
 
 test_content = "This a sentence\n which should be on\n one line."
 
-expected_output = ['This a sentence\n', ' which should be on\n', ' one line.']
+expected_output = 'This a sentence\n which should be on\n one line.'
 
 def test_one_line_dialogue():
 	with open("test_dialogue_file.txt", 'w') as test_file:
